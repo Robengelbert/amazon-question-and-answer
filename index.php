6055585f13cef074ca336bb70d4037877479e6dd
Robert Engelbert <Robe32002@yahoo.com>
2016-07-15 08:28:27 -0600

    Added basic html with comments for what needs to be done.

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Amazon question and answer</title>
+	</head>
+	<body>
+		<h1>Customer Questions & Answers </h1>
+		<!-- need a place at the top of the form for users to search for answers already in the database of questions or ask a new question if there question doesn't already exist in the database. -->
+		<!-- Need to post questions with the ability for other users to answer the question -->
+		<!-- Need to post answers -->
+		<!-- Need to have option for up or down votes -->
+	</body>
+</html>
