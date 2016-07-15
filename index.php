d311e502422bb8b006a57f265fa21d0c7546edb6
Robert Engelbert <Robe32002@yahoo.com>
2016-07-15 09:16:00 -0600

    Added a persona for my amazon questions project.

--- index.php
+++ index.php
@@ -7,8 +7,16 @@
 	<body>
 		<h1>Customer Questions & Answers </h1>
 		<!-- need a place at the top of the form for users to search for answers already in the database of questions or ask a new question if there question doesn't already exist in the database. -->
+		<h2>Persona</h2>
+		<p><strong>Name:</strong>Robert Engelbert</p>
+		<p><strong>Age:</strong>32</p>
+		<p><strong>Profession:</strong>Web developer and Student at deep dive coders bootcamp.</p>
+		<p><strong>Technology:</strong>Robert uses an HP Notebook at school and at home. Robert has a 5 dollar pre-paid flip phone from the dollar store.</p>
+		<p><strong>Needs:</strong>Robert needs a data phone to help test the mobile sites he creates.</p>
+		<p><strong>Goals:</strong>By looking at the questions and awnsers, Roberts goal is to weed through the problem phones and find a good phone at a good price.</p>
 		<!-- Need to post questions with the ability for other users to answer the question -->
 		<!-- Need to post answers -->
 		<!-- Need to have option for up or down votes -->
+		<p>To see the page this is designed after <a href="https://www.amazon.com/Huawei-Nexus-6P-unlocked-smartphone/dp/B019TWO6WM/ref=sr_1_14?s=wireless&ie=UTF8&qid=1468535132&sr=1-14&keywords=nexus+6">click here.</a></p>
 	</body>
 </html>
