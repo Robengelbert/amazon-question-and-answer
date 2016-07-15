73bee534a0d83f46226edc48a0b7cd0685598a4c
Robert Engelbert <Robe32002@yahoo.com>
2016-07-15 12:12:39 -0600

    Added a Use Case, Interaction Flow, and Conceptual Model.

--- index.php
+++ index.php
@@ -13,10 +13,24 @@
 		<p><strong>Profession:</strong>Web developer and Student at deep dive coders bootcamp.</p>
 		<p><strong>Technology:</strong>Robert uses an HP Notebook at school and at home. Robert has a 5 dollar pre-paid flip phone from the dollar store.</p>
 		<p><strong>Needs:</strong>Robert needs a data phone to help test the mobile sites he creates.</p>
-		<p><strong>Goals:</strong>By looking at the questions and awnsers, Roberts goal is to weed through the problem phones and find a good phone at a good price.</p>
+		<p><strong>Goals:</strong>By looking at the questions and answers, Roberts goal is to weed through the problem phones and find a good phone at a good price.</p>
+		<h2>Use case</h2>
+		<p>Robert has got on to Amazon, navigated to the data phones and clicked on the link for a Nexus 6. Robert is interested, but the phone is a little pricey. Robert scrolls down the page, and stops on the customer question and answer section. There's a search bar at the top of the Q&A section of the page to search the existing questions or ask new questions if your question doesn't exist. There are 4 questions with answers displayed. There are up and down votes to vote on how helpful te answer to the question is. finaly at the bottom of the page theres a button to view all previous posts. Robert searches the post to answer the questions he has about the phone.</p>
+		<h2> Interaction Flow</h2>
+		<ol>
+			<li>Roberts main goal in visiting <a href="https://www.amazon.com/?ie=UTF8&hvadid=3480083188&hvbmt=be&hvdev=c&hvqmt=e&ref=pd_sl_7j18redljw_e&tag=hymsabk-20">Amazon.com</a> is to research data phones</li>
+			<li>Robert needs to research data phones for his new career as a web developer.</li>
+			<li>Robert will need to pick a phone and purchase the phone before his first interview with a potential employer.</li>
+			<li>Robert will search Amazon from home on his HP Notebook.</li>
+		</ol>
+		<h2>Conceptual Model</h2>
+		<ol>
+			<li>Each user can veiw <strong> many</strong> questions.</li>
+			<li>Each user can post <strong>many</strong> questions.</li>
+			<li>Each user can search and vote up or down on <strong>many</strong>questions.</li>
+		</ol>
 		<!-- Need to post questions with the ability for other users to answer the question -->
 		<!-- Need to post answers -->
 		<!-- Need to have option for up or down votes -->
-		<p>To see the page this is designed after <a href="https://www.amazon.com/Huawei-Nexus-6P-unlocked-smartphone/dp/B019TWO6WM/ref=sr_1_14?s=wireless&ie=UTF8&qid=1468535132&sr=1-14&keywords=nexus+6">click here.</a></p>
 	</body>
 </html>
