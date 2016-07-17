0a8a941a99d085d751942fca3d9a18d1b7bb67db
Robert Engelbert <Robe32002@yahoo.com>
2016-07-17 07:57:16 -0600

    Added strong tags for bold text.

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 		<p><strong> Goals:</strong>By looking at the questions and answers, Roberts goal is to weed through the problem phones and find a good phone at a good price.</p>
 		<!-- Document a use case -->
 		<h2>Use case</h2>
-		<p>Robert has got on to <a href="https://www.amazon.com/?ie=UTF8&hvadid=3480083188&hvbmt=be&hvdev=c&hvqmt=e&ref=pd_sl_7j18redljw_e&tag=hymsabk-20">Amazon.com</a>, navigated to the data phones and clicked on the link for a<a href="https://www.amazon.com/Huawei-Nexus-6P-unlocked-smartphone/dp/B019TWO6WM/ref=sr_1_14?s=wireless&ie=UTF8&qid=1468535132&sr=1-14&keywords=nexus+6"> Nexus 6 </a>. Robert is interested, but the phone is a little pricey. Robert scrolls down the page, and stops on the customer question and answer section. There's a search bar at the top of the Q&A section of the page to search the existing questions or ask new questions if your question doesn't exist. There are 4 questions with answers displayed. There are up and down votes to vote on how helpful the answer to the question is. finally at the bottom of the page there's a button to view all previous posts. Robert searches the post to answer the questions he has about the phone.</p>
+		<p>Robert has got on to <a href="https://www.amazon.com/?ie=UTF8&hvadid=3480083188&hvbmt=be&hvdev=c&hvqmt=e&ref=pd_sl_7j18redljw_e&tag=hymsabk-20">Amazon.com</a>, navigated to the data phones and clicked on the link for a<a href="https://www.amazon.com/Huawei-Nexus-6P-Unlocked-Smartphone/dp/B015YCRZ0G/ref=sr_1_9?s=wireless&ie=UTF8&qid=1468757622&sr=1-9&keywords=nexus+6"> Nexus 6 </a>. Robert is interested, but the phone is a little pricey. Robert scrolls down the page, and stops on the customer question and answer section. There's a search bar at the top of the Q&A section of the page to search the existing questions or ask new questions if your question doesn't exist. There are 4 questions with answers displayed. There are up and down votes to vote on how helpful the answer to the question is. finally at the bottom of the page there's a button to view all previous posts. Robert searches the post to answer the questions he has about the phone.</p>
 		<!-- State the interaction flow -->
 
 		<h2> Interaction Flow</h2>
@@ -38,7 +38,8 @@
 		<h2>Conceptual Model</h2>
 		<ol>
 			<li>Each user can post 1 post <strong>many</strong> times with different questions in each post.</li>
-			<li><strong>Many</strong> users can respond to each post many times</li>
+			<li><strong>Many</strong> users can respond to each post <strong>many</strong> times</li>
+			<li>The seller can respond each question <strong>many</strong> times</li>
 			<li>Each user can view <strong> many</strong> questions <strong>many</strong> times.</li>
 			<li>Each user can search and vote up or down on <strong>many</strong>questions <strong>many</strong> times.</li>
 		</ol>
